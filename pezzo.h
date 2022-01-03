d14b5c0ffce770da23da1bd354e77b6936f53fcb
Filippo <morbiatofilippo@gmail.com>
2022-01-03 22:24:03 +0100

    Pezzo
    Aggiornamento classe Pezzo

--- pezzo.h
+++ pezzo.h
@@ -1,10 +1,55 @@
+#ifndef PEZZO_H
+#define PEZZO_H
+
 class Pezzo{
-    private:
-    bool colore;    //true=bianco,false=nero
-    bool stato;     //true=vivo, false=morto
-    char nome;
+    protected:
+    char name;  //nome del Pezzo
+    bool color;  //colore del pezzo("true"=white, "false"=black)
+
     public:
-    Pezzo(bool colore);
-    void mossa();
-    char getNome();
+    virtual ~Pezzo();  //distruttore 
+    char getName() const;  //nome del Pezzo
+    bool getColor() const;  //colore del Pezzo
+    int getPosition();  //posizione del pezzo
+    void setPosition();  //sposta il pezzo 
+    virtual bool isValid(/*...*/) = 0;  //check di validità delle mosse(funzione virtuale pura)
+
 };
+
+class Pedone : public Pezzo{
+    public:
+    Pedone(bool color);
+    bool isValid(/*...*/);
+};
+
+class Torre : public Pezzo{
+    public:
+    Torre(bool color);
+    bool isValid(/*...*/);
+};
+
+class Alfiere : public Pezzo{
+    public:
+    Alfiere(bool color);
+    bool isValid(/*...*/);
+};
+
+class Cavallo : public Pezzo{
+    public:
+    Cavallo(bool color);
+    bool isValid(/*...*/);
+};
+
+class Re : public Pezzo{
+    public:
+    Re(bool color);
+    bool isValid(/*...*/);
+};
+
+class Regina : public Pezzo{
+    public:
+    Regina(bool color);
+    bool isValid(/*...*/);
+};
+
+#endif
