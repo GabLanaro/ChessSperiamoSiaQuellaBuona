079f40134076deea042a628859ca2cf522295686
GabLanaro <gabriel.lanaro@studenti.unipd.it>
2022-01-07 12:04:02 +0100

    stampa è

--- game.cpp
+++ game.cpp
@@ -30,23 +30,23 @@ void Game::start()
         player1.setTipo(true); // player1 è umano
         if (player1.getColor() == 0)
         {
-            std::cout << "giocatore umano, ti è stato assegnato il colore nero,\ncomputer, muoverai la prima mossa.\n";
+            std::cout << "giocatore umano, ti \212 stato assegnato il colore nero,\ncomputer, muoverai la prima mossa.\n";
         }
         else
         {
-            std::cout << "giocatore umano, ti è stato assegnato il colore bianco,\nmuoverai la prima mossa.\n";
+            std::cout << "giocatore umano, ti \212 stato assegnato il colore bianco,\nmuoverai la prima mossa.\n";
         }
     }
     Player *currentPlayer = (player1.getColor() == true) ? &player1 : &player2; // currentPlayer è un puntatore al player bianco
     // CICLO CHE VERIFICA SCACCO E SCACCOMATTO, CHIEDE MOSSA, LA EFFETTUA E CAMBIA TURNO.
     do
     {
-        if (sottoScacco(*currentPlayer))
+        if (sottoScacco(*currentPlayer, board))
         {
             if (scaccoMatto(*currentPlayer))
                 gameIsOver = true;
             else
-                std::cout << "attenzione! Il tuo re è sottoscacco." << std::endl;
+                std::cout << "attenzione! Il tuo re \212 sottoscacco." << std::endl;
         }
         // il re non è sottoscacco, currentPlayer inserisce mossa.
         bool done = false;
@@ -57,8 +57,7 @@ void Game::start()
             if (board.isMoveValid(rigaI, colonnaI, rigaF, colonnaF))
             { // verifica della correttezza della mossa inserita
                 board.spostaPezzo();
-                std::cout << "mossa effettuata:   " << rigaI << "-" << colonnaI
-                          << " " << rigaF << "-" << colonnaF << "\n";
+                std::cout << "mossa effettuata:   " << colonnaI << rigaI << " " << colonnaF << rigaF << "\n";
                 fileLog << rigaI << colonnaI << " " << rigaF << colonnaF << "\n";
                 currentPlayer = (currentPlayer->getColor() == player1.getColor()) ? &player2 : &player1; // cambio turno giocatore
                 done = true;                                                                             // esci dal ciclo
@@ -86,47 +85,47 @@ bool Game::randomColor()
 }
 
 bool Game::sottoScacco(Player &p, Board &b)
-{
-    // cerco il re del currentPlayer
-    // for annidato per leggere la board
-    // if !=nullPtr    if getPezzo della posizione corrente == Re
-    // getPezzo returna la posizione del puntatore al pezzo ovvero board[riga][colonna]
-    // vedo se i pezzi dell'avversario riescono a mangiarlo
+{ /*
+     // cerco il re del currentPlayer
+     // for annidato per leggere la board
+     // if !=nullPtr    if getPezzo della posizione corrente == Re
+     // getPezzo returna la posizione del puntatore al pezzo ovvero board[riga][colonna]
+     // vedo se i pezzi dell'avversario riescono a mangiarlo
 
-    //PRIMA PARTE: Cerco il Re del current Player
-    bool trovato = false;
-    int rigRe;
-    int colRe;
-    while (!trovato)
-    { // Scorro tutte le caselle della scacchiera
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                // ispeziono il pezzo preso per vedere se è il re cercato
-                Pezzo *pez = b.getPezzo(i, j);
-                if (b.getPezzo(i, j) != NULL)
-                {
-                }
-                if (p.getColor() == pez->getColor())
-                {
-                    if (pez->getName() == 'k' && p.getColor() == true) // se bianco
-                    {
-                        trovato = true;
-                        rigRe = i;
-                        colRe = j;
-                    }
-                    else if (pez->getName() == 'K' && p.getColor() == false) // se nero
-                    {
-                        trovato = true;
-                        rigRe = i;
-                        colRe = j;
-                    }
-                }
-            }
-        }
-    }
-    //SECONDA PARTE: Vedo se i pezzi avversari possono mangiare il re trovato
+     // PRIMA PARTE: Cerco il Re del current Player
+     bool trovato = false;
+     int rigRe;
+     int colRe;
+     while (!trovato)
+     { // Scorro tutte le caselle della scacchiera
+         for (int i = 0; i < 8; i++)
+         {
+             for (int j = 0; j < 8; j++)
+             {
+                 // ispeziono il pezzo preso per vedere se è il re cercato
+                 Pezzo *pez = b.getPezzo(i, j);
+                 if (b.getPezzo(i, j) != NULL)
+                 {
+                 }
+                 if (p.getColor() == pez->getColor())
+                 {
+                     if (pez->getName() == 'k' && p.getColor() == true) // se bianco
+                     {
+                         // trovato = true;
+                         rigRe = i;
+                         colRe = j;
+                     }
+                     else if (pez->getName() == 'K' && p.getColor() == false) // se nero
+                     {
+                         // trovato = true;
+                         rigRe = i;
+                         colRe = j;
+                     }
+                 }
+             }
+         }
+     }*/
+    // SECONDA PARTE: Vedo se i pezzi avversari possono mangiare il re trovato
 
     return false;
 }
--- scacchiera.cpp
+++ scacchiera.cpp
@@ -2,17 +2,20 @@
 #include <string>
 #include "game.h"
 using namespace std;
-int main(int argc, char* argv[]){
+
+int main(int argc, char *argv[])
+{
     string modalita{argv[1]};
-    //controllo che l'argomento inserito da linea di comando sia valido
-    if(modalita!="cc" && modalita!="pc"){
-        cout<<"La modalità di gioco inserita non è valida,\nSi riesegua il programma usando come argomento \"pc\" o \"cc\"\n";
+    // controllo che l'argomento inserito da linea di comando sia valido
+    if (modalita != "cc" && modalita != "pc")
+    {
+        cout << "La modalita' di gioco inserita non \212 valida,\nSi riesegua il programma usando come argomento \"pc\" o \"cc\"\n";
         return 0;
     }
-    cout<<"*******************************************\n";
-    cout<<"Benvenuto al gioco degli scacchi\n";
-    cout<<"La modalità di gioco scelta è: "<<argv[1]<<endl;
-    Game gioco(modalita);    //creazione del gioco
+    cout << "*******************************************\n";
+    cout << "Benvenuto al gioco degli scacchi\n";
+    cout << "La modalita' di gioco scelta \212: " << argv[1] << endl;
+    Game gioco(modalita); // creazione del gioco
     gioco.start();
     return 0;
 }
